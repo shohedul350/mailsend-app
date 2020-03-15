109936ed6cc62ea4c24efe72282387716c5e6e10
shohedul350 <shohedulislamemon@gmail.com>
2020-03-15 22:51:31 +0600

    complete crete server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
+
+
+// routes
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+app.use('/api', require());
+
+app.listen(PORT, () => { console.log(`The app is running on port ${PORT}`); });
